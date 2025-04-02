bf3e99956135b05da27d7fdd29d227e7b1b5b407
Mats Holmberg <50166211+mattekholm@users.noreply.github.com>
2025-04-02 12:02:47 +0300

    remove failure simulation

--- app.js
+++ app.js
@@ -18,6 +18,4 @@ app.get('/health', (req, res) => {
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
-  
-  log('simulated error'
 })
